c3262e6e6e2aea6295c69d43ef98d8180b5e4950
Renzo Espinola <renzo.espinola87@gmail.com>
2022-04-20 20:07:20 -0300

    Clase 2

--- python/Leccion 1/main.py
+++ python/Leccion 1/main.py
@@ -1,13 +1,38 @@
 miVariable = 3
 print(miVariable)
-miVariable="Hola a todos los Estudiantes de la tecnicatura"
+miVariable = "Hola a todos los Estudiantes de la tecnicatura"
 print(miVariable)
-miVariable=3.5
+miVariable = 3.5
 print(miVariable)
-x=10
-y=2
-z=x+y
+x = 10
+y = 2
+z = x + y
 print(id(x))
-#las literales se escriben x528, la variable y = x272, la variable z= x592
+# las literales se escriben x528, la variable y = x272, la variable z= x592
 print(id(y))
 print(id(z))
+# Las variables en python son dinamicas pueden adoptar diferentes tipos
+a = "Hola Alumnos"
+print(type(a))
+a = 10.78
+print(type(a))
+b = True
+print(type(b))
+b = False
+print(type(b))
+# Tipos int, float, String, Bool
+x = 10
+print(x)
+print(type(x))
+x = 14.5
+print(x)
+print(type(x))
+x = "Hola ALumnos"
+print(x)
+print(type(x))
+x = True
+print(x)
+print(type(x))
+x = False
+print(x)
+print(type(x))
