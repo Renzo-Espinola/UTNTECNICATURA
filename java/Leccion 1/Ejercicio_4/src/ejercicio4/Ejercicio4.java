85ee54f7e641f2db10de704037fd65b72bcc3b10
Renzo Espinola <renzo.espinola87@gmail.com>
2022-06-10 22:02:23 -0300

    actualizacion de todos los proyectos

--- java/Leccion 1/Ejercicio_4/src/ejercicio4/Ejercicio4.java
+++ java/Leccion 1/Ejercicio_4/src/ejercicio4/Ejercicio4.java
@@ -1,2 +1,14 @@
-package ejercicio4;public class Ejercicio4 {
+package ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un numero: ");
+        var num1 = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite otro numero: ");
+        var num2 = Integer.parseInt(entrada.nextLine());
+        System.out.println(num1>num2?"El mayor es: "+num1:"El mayor es: "+num2);
+    }
 }
