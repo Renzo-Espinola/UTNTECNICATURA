cf97bb3d93e92bd4d0eb37df65073ec3e0feab5a
Renzo Espinola <renzo.espinola87@gmail.com>
2022-08-26 19:56:58 -0300

    commit leccion 2 python

---
+++ Python/Leccion4/Ejercicio1.py
@@ -0,0 +1,16 @@
+
+print("Ejercicio 1")
+for numero in range(11):
+    if numero % 3 == 0:
+        print(numero)
+
+# Ejercicio 2
+print("Ejercicio 2")
+
+for numero in range(2,7):
+    print(numero)
+
+print("Ejercicio 3")
+
+for numero in range(3,11,2):
+    print(numero)
