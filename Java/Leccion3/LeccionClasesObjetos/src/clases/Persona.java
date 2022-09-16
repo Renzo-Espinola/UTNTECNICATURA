e6c794775ccefbf6bb3ca657cca9cb52503dbcd4
Renzo Espinola <renzo.espinola87@gmail.com>
2022-09-16 20:14:19 -0300

    commit leccion 3 clases y objetos JAVA

--- Java/Leccion3/LeccionClasesObjetos/src/clases/Persona.java
+++ Java/Leccion3/LeccionClasesObjetos/src/clases/Persona.java
@@ -1,4 +1,11 @@
 package clases;
 
 public class Persona {
+    String nombre;
+    String apellido;
+    Persona(){}
+    public void obtenerInformacion() {
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
