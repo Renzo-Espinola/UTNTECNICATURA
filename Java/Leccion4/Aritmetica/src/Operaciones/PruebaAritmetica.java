eafe8a3b80037aa3dbfb9125efbda7f70649df76
Renzo Espinola <renzo.espinola87@gmail.com>
2022-10-14 21:05:58 -0300

    commit clase 14/10 java encapsulamiento

--- Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -3,9 +3,9 @@ package Operaciones;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         //variables locales
-        int a =10;
+        int a =10;// variables locales
         int b =7;//MEMORIA STACK
-        miMetodo();
+        miMetodo();//llamamos al metodo nuevo
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a=3;
         aritmetica1.b=7;
@@ -24,11 +24,37 @@ public class PruebaAritmetica {
         System.out.println("aritmetica2.a = " + aritmetica2.a);
         System.out.println("aritmetica2.b = " + aritmetica2.b);
         //GARBAGE COLLECTOR:
-        System.gc();
+        //System.gc();
+        Persona persona = new Persona("Ariel","Bentancoud");
+        System.out.println("persona = " + persona);
+        System.out.println("Persona nombre:"+persona.nombre);
+        System.out.println("Persona apellido:"+persona.apellido);
     }
-
+    //
     public static void miMetodo(){
         //int a=10;
         System.out.println("Aqui hay otro metodo");
+    }
+}
+class Persona{
+    String nombre;
+    String apellido;
+    Persona (String nombre, String apellido) {//Constructor
+        super();//llamada al constructor de la clase padre object
+        //Imprimir imprimir = new Imprimir();
+        new Imprimir().imprimir(this);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        System.out.println("Objeto Persona usando this: " + this);
+    }
+
+}
+class Imprimir{
+    public Imprimir(){
+        super();
+    }
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde la clase imprimir: "+persona);
+        System.out.println("Impresion del objeto actual (this): "+ this);
     }
 }
