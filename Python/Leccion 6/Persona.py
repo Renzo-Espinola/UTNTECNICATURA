7dca9cd33db0f42feb0edbbb71413c30f6c2d741
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-18 22:21:47 -0300

    commit python clase 10 (#32)

--- Python/Leccion 6/Persona.py
+++ Python/Leccion 6/Persona.py
@@ -1,8 +1,13 @@
 class Persona: # Creamos una clase
-    def __init__(self,nombre,apellido,edad): # se lo llama metodo Init dunder
+    def __init__(self, nombre, apellido, edad, args, kwargs): # se lo llama metodo Init dunder
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
+        self.args = args
+        self.wkargs = kwargs
+
+    def mostrar_detalle(self): # self es igual a this
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
 
 persona1=Persona('Renzo','Espinola',34) # necesitamos enviar argumentos
 print(type(Persona))
@@ -21,4 +26,8 @@ print(f'El objeto1 de la clase persona: {persona1.nombre} {persona1.apellido} su
 
 # Los atributos son las caracteristicas
 # Los metodos son el comportamiento que van a tener los objetos (acciones)
+persona1.mostrar_detalle() # La referencia en este caso se pasa de manera automatica
+persona2.mostrar_detalle()
+
+#Persona.mostrar_detalle(persona1) # Debemos pasarle una referencia para el self o dara error
 
