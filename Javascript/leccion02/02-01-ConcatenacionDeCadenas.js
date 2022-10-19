dde60b3129eca91cb926d9643c15b85e7ee77ceb
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-18 22:23:14 -0300

    dev into master (#34)
    * commit python clase 10
    
    * Commit clase 10 JavaScript Concatenacion de Cadenas (#33)

---
+++ Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,19 @@
+var nombre = "Jose";
+var apellido = " Montes";
+var nombreCompleto = nombre + " "+apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = "Renzo" + " " +"Espinola"
+console.log(nombreCompleto2)
+
+var juntos = nombre + 219;
+console.log(juntos);
+juntos = nombre +78 +17;
+console.log(juntos);
+
+juntos = 78 +17 + nombre;
+console.log(juntos); //Contexto String
+
+nombre += apellido;
+console.log(nombre);
+
