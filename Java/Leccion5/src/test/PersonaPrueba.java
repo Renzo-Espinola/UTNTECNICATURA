9edf21645e849471bfebb3242348afbe7940d36a
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-25 20:41:42 -0300

    Develop into master (#35)
    * Commit ejercicio 10 joptionpane
    
    * commit clase 11 python
    
    * Commit Clases de ejercicios de java
    
    * Leccion 08 de Python

--- Java/Leccion5/src/test/PersonaPrueba.java
+++ Java/Leccion5/src/test/PersonaPrueba.java
@@ -10,7 +10,17 @@ public class PersonaPrueba {
         System.out.println("persona 1 con su nombre modificado es: " + persona1.getNombre());
         System.out.println("persona1 su sueldo es: "+persona1.getSueldo());
         System.out.println("persona1.isEliminado() = " + persona1.isEliminado());
-        //Tarea: crear otro objeto de tipo Persona, asignar valores de manear inicial
+        //Tarea: crear otro objeto de tipo Persona, asignar valores de manera inicial
         // e imprimir luego modificar sus valroes y volver a imprimir
+        Persona persona2 = new Persona("Tatiana",45000,false);
+        System.out.println("persona 1 con su nombre modificado es: " + persona2.getNombre());
+        System.out.println("persona1 su sueldo es: "+persona2.getSueldo());
+        System.out.println("persona1 esta Eliminado? = " + persona2.isEliminado());
+        persona2.setNombre("Athena");
+        persona2.setSueldo(89000);
+        persona2.setEliminado(true);
+        System.out.println("persona 2 con su nombre modificado es: " + persona1.getNombre());
+        System.out.println("persona 2 su sueldo es: "+persona1.getSueldo());
+        System.out.println("persona 2  esta Eliminado? = " + persona1.isEliminado());
     }
 }
