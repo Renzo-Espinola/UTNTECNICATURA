c282723fb8d49d2ace09550a147512eb2f73ad31
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-25 20:44:14 -0300

    Leccion 08 de Python

---
+++ Python/Leccion08/Vehiculo.py
@@ -0,0 +1,20 @@
+class Vehiculo:
+'''
+Definir una clase pagre llamada vehiculo y dos clases hijas llamadas autos y bicletas
+las cuales heredan de la clase padre vehiculo la clase pagre debe tener los siguientes atributos
+y metodos:
+
+Vehiculo (clase padre)
+-atributos(color,ruedas)
+-metodos(__init__(color, ruedas) y __str__())
+
+Auto(clase hija de vehiculo)
+-atributos(velocidad (km/hr))
+-metodos(__init__(color,ruedas, velocidad) y __str__())
+
+Bicicleta(clase hija de vehiculo)
+--atributos(tipo(urbana/montaña/etc))
+--metodos(__init__(color,ruedas,tipo) y __str__())
+crear un objeto de cada clase
+
+'''
