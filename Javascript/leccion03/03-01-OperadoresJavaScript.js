958e457c471512be6d6837924feb179f1c097324
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-25 21:45:39 -0300

    commit leccion 3 Javascript

---
+++ Javascript/leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,19 @@
+// Ejercicios para encontrar numeros pares
+
+let num = 4;
+
+if(num % 2 == 0){
+    console.log("Numero par");
+}else{
+    console.log("Numero impar");
+}
+
+//Encontrar mayor de edad
+
+let edad = 21
+
+if (edad < 18){
+    console.log("Menor de edad: "+edad);
+}else{
+        console.log("Mayor de edad: "+edad)
+    }
