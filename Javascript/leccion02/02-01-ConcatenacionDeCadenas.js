ef21ae5960b6bc0f68f22f4a0753fc62d3908d66
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-10-25 21:46:51 -0300

    commit leccion 3 Javascript (#37)

--- Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -16,4 +16,32 @@ console.log(juntos); //Contexto String
 
 nombre += apellido;
 console.log(nombre);
+//hoy ya no se utiliza var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2="lepes";
+//apellido2 es una constante no se puede modificar
+console.log(apellido2);
+
+let x, y;
+x = 17, y = 21; 
+let z = x + y;
+console.log(z)
+
+//let 1num = 31; no se permite crear una variabla con un numero al principio
+
+let $1num = 31;
+let A1num = 54;
+let _1num = 54;
+
+//let break = 23 no se puede usar palabras reservadas
+let rompiendo = "rompe"
+
+console.log(_1num);
+console.log(rompiendo);
+
+
+
+
 
