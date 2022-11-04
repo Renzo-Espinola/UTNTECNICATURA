a0547bc9f3ee509a2dfc4a60797a91c5ec0b9957
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-11-04 19:44:46 -0300

    Clase herencia JAVA

---
+++ Java/Leccion6/src/domain/Empleado.java
@@ -0,0 +1,38 @@
+package domain;
+
+public class Empleado extends Persona{
+    private int idEmpleado;
+    private double sueldo;
+
+    private static int contadorEmpleados;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, double sueldo) {
+        super(nombre);
+        this.idEmpleado=++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Empleado{");
+        sb.append("idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append('}');
+        return sb.toString();
+    }
+}
