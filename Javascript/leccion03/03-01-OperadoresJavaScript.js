4480cd0e3b5604fc8127aee640fbac44102f89aa
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2022-11-15 21:20:12 -0300

    Clase 10 ejercicios (#43)

--- Javascript/leccion03/03-01-OperadoresJavaScript.js
+++ Javascript/leccion03/03-01-OperadoresJavaScript.js
@@ -17,3 +17,15 @@ if (edad < 18){
 }else{
         console.log("Mayor de edad: "+edad)
     }
+
+    //Ejercicio: dentro de un rango
+
+let dentroRango = 5; //Aqui vamos ir cambiando el valor
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log('Esta dentro del rango establecido')
+}
+else{
+    console.log('Esta fuera del rango')
+}
+
